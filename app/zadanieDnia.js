051814dfbb518e82399052b6404dcc7193d3ef95
Styku0 <styku94@gmail.com>
2018-09-19 12:05:12 +0200

    Zadania rozwiązane

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,7 @@
 //Twój kod
+const numbers = [];
+for(let i = 2; i < process.argv.length; i++){
+    numbers.push(Number(process.argv[i]));
+}
+
+numbers.forEach(element => setTimeout(()=> console.log(element), element*1000));
